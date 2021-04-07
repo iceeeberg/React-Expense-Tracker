848c8a3d22c0b0b145cc22845cbfc9212aba95f9
iceeeberg <72841352+iceeeberg@users.noreply.github.com>
2021-04-06 19:13:07 -0500

    third commit, experimenting with form submission

--- src/App.js
+++ src/App.js
@@ -5,20 +5,42 @@ import Form from "./Components/Form";
 import Table from "./Components/Table";
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {amount: '', 
+                  date: '',
+                  location: '', 
+                  description: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  render() {
-    return (
+    handleChange(event) {
+      this.setState({amount: event.target.value,
+                      date: event.target.value,
+                      location: event.target.value,
+                      description: event.target.value
+                    })
+                    
+    }
+
+    handleSubmit(event) { 
+      event.preventDefault();
+      alert("An expense was submitted" + this.state.value)
+    }
+    
+    render (){
+      return (
       <div className="App">
         <header>
           <h1>Expense Tracker</h1>
         </header>
-        <Form  />
+        <Form handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
         <br></br>
        <Table />
       </div>
     );
   }
-  }
- 
+}
 
 export default App;
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,44 +1,18 @@
 import React, {Component} from "react"
 
 class Form extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      amount: null,
-      date: null,
-      description: null, 
-      location: null
-    }
-  }
-
-  handleSubmit = (event) =>{
-    event.preventDefault()
-    const expense = this.state
-    console.log(expense)
-  }
-
-  handleInputChange = (event) => {
-    this.setState({
-      [event.target.amount]: event.target.value,
-      [event.target.date]: event.target.value,
-      [event.target.location]: event.target.value,
-      [event.target.description]: event.target.value,
-    })
-  }
-  
-  render() {
-      const {amount} = this.state
+    render(){
     return (
       <div>
-      <form onSubmit={this.handleSubmit} id="form">
+      <form onSubmit={this.props.handleSubmit} id="form">
         <label amount="amount">Enter Amount:</label>
-          <input onChange={this.handleInputChange} type="number" placeholder="Enter Amount.."></input>
+          <input onChange={this.props.handleInputChange} type="number" placeholder="Enter Amount.."></input>
         <label date="date">Enter Date:</label>
-          <input onChange={this.handleInputChange} type="date"></input>
+          <input onChange={this.props.handleInputChange} type="date"></input>
         <label location ="location">Enter Location:</label>
-          <input onChange={this.handleInputChange}  type="text" placeholder="Enter Location.."></input>
+          <input onChange={this.props.handleInputChange}  type="text" placeholder="Enter Location.."></input>
         <label description="description">Enter Description:</label>
-          <input onChange={this.handleInputChange} type="text"placeholder="Enter Description.."></input>
+          <input onChange={this.props.handleInputChange} type="text"placeholder="Enter Description.."></input>
         <br></br>
           <input id="submit" type="button" value="Submit Expense"></input>
         </form>
@@ -46,5 +20,4 @@ class Form extends Component{
       )
     }
   }
-
   export default Form
